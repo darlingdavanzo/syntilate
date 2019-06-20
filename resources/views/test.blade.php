8383c49867e9b4608f00fc4c22cb0e26cc3fce46
Darling D'Avanzo <stormofdark@libero.it>
2019-06-20 20:36:53 +0200

    Made some sections

---
+++ resources/views/test.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+@section('title', 'Welcome')
+
+@section('styles')
+  <link rel="stylesheet" href="{{ @asset('css/landing.css') }}">
+  <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i|Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+@endsection
+
+@section('content')
+  <div id="particles-js"></div>
+  <div class="container-fluid h-100">
+    <div class="row h-100 bg-danger"></div>
+    <div class="row h-100 bg-warning"></div>
+    <div class="row h-100 bg-info"></div>
+  </div>
+@endsection
+
+@section('scripts')
+  <script src="{{ @asset('js/particles.min.js') }}"></script>
+  <script src="{{ @asset('js/particles.js') }}"></script>
+  <script src="{{ @asset('js/landing.js') }}"></script>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,9 @@
 
 @section('content')
 	<div id="particles-js"></div>
-	<div id="home" class="container-fluid h-100">
-		<div class="row h-100">
-			<div class="offset-2 col-sm-8 h-100 d-flex flex-column align-items-center justify-content-center">		
+	<div  class="container-fluid h-100">
+		<div id="home" class="row h-100">
+			<div class="offset-sm-2 col-sm-8 h-100 d-flex flex-column align-items-center justify-content-center">		
 				<img class="img-fluid" src="{{ @asset('assets/images/Syntilate.png') }}" width="">		
 				<h1 class="text-white header-title text-center">Committed to <strong><i>greatness</i></strong></h1>
 				<p class="header-text text-center">
@@ -23,67 +23,63 @@
 			</div>
 		</div>
 		<div id="whatwedo" class="row h-100">
-
-			<div class="col-sm" style="background: url({{ @asset('assets/images/1052.jpg')  }}); background-size: cover">
-				
+			<div class="col-sm-12" style="background-color: #42a832">
+				<h1 class="text-white header-title text-center pt-5">What we do ?</h1>
+				<div class="container" style="padding: 30px; background-color: rgb(255,255,255,0.2); border-radius: 10px;">
+					<div class="row">
+						<div class="col-sm text-center">
+							<img src="https://image.freepik.com/vettori-gratuito/interfaccia-hud-futuristica-con-stile-sfumato_23-2147907638.jpg" class="rounded-circle img-fluid" alt="" width="40%"/>
+						</div>
+						<div class="col-sm">
+							<h4 class="text-white text-center">Find rewarding projects</h4>
+							<p class="text-white text-center">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations. Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work.</p>
+						</div>
+					</div>
+					<div class="row pt-5">
+						<div class="col-sm">
+							<h4 class="text-white text-center">Find rewarding projects</h4>
+							<p class="text-white text-center">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations. Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work. More and more success. .</p>
+						</div>
+						<div class="col-sm text-center">
+							<img src="https://image.freepik.com/vettori-gratuito/interfaccia-hud-futuristica-con-stile-sfumato_23-2147909276.jpg" class="rounded-circle img-fluid" alt="" width="40%"/>
+						</div>
+					</div>				
+					<div class="row pt-5">
+						<div class="col-sm text-center">
+							<img src="https://image.freepik.com/vettori-gratuito/interfaccia-hud-futuristica-con-stile-sfumato_23-2147914520.jpg" class="rounded-circle img-fluid" alt="" width="40%" />
+						</div>
+						<div class="col-sm">
+							<h4 class="text-white text-center">Find rewarding projects</h4>
+							<p class="text-white text-center">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations. Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work. More and more success. </p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<div id="ourstack" class="row h-100">
-
-			<div class="col-sm" style="background: url({{ @asset('assets/images/18783.jpg')  }}); background-size: cover">
-				
-			</div>
+				<div class="col-sm bg-white" style="background: url({{ @asset('assets/images/19276.jpg')  }}); background-size: cover">
+					<div class="offset-sm-8 col-sm-4 ">					
+						<h1 class="text-dark header-title text-center pt-5">About us</h1>
+						<p class="text-dark text-center">Freedom to work on ideal projects. On GetLance, you run your own business and choose your own clients and projects. Just complete your profile and we’ll highlight ideal jobs. Also search projects, and respond to client invitations. Wide variety and high pay. Clients are now posting jobs in hundreds of skill categories, paying top price for great work. More and more success. </p>		
+						
+						<h1 class="text-dark header-title text-center pt-5">Contact us</h1>
+						<form action="#">
+							<div class="form-group">
+								<input class="form-control" type="text" placeholder="Name">
+							</div>
+							<div class="form-group">
+								<input class="form-control" type="text" placeholder="Name">
+							</div>
+							<div class="form-group">							
+								<textarea class="form-control" type="text" placeholder="Name"></textarea>
+							</div>								
+						</form>
+					</div>
+				</div>
+			
 		</div>
-		<div class="row" style="margin-top: 50px; margin-bottom: 50px">
-			<div class="offset-2 col-sm-2">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Software Development</h5>
-						<p class="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae odio neque. Phasellus massa leo, bibendum sit amet blandit et, tincidunt dignissim erat. Nunc risus nulla, lobortis quis sapien id, laoreet rutrum odio. Donec eleifend viverra tellus, nec tincidunt justo congue eu.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-2">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Software Development</h5>
-						<p class="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae odio neque. Phasellus massa leo, bibendum sit amet blandit et, tincidunt dignissim erat. Nunc risus nulla, lobortis quis sapien id, laoreet rutrum odio. Donec eleifend viverra tellus, nec tincidunt justo congue eu.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-2">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Software Development</h5>
-						<p class="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae odio neque. Phasellus massa leo, bibendum sit amet blandit et, tincidunt dignissim erat. Nunc risus nulla, lobortis quis sapien id, laoreet rutrum odio. Donec eleifend viverra tellus, nec tincidunt justo congue eu.
-						</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-sm-2">
-				<div class="card">
-					<div class="card-body">
-						<h5 class="card-title">Software Development</h5>
-						<p class="card-text">
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae odio neque. Phasellus massa leo, bibendum sit amet blandit et, tincidunt dignissim erat. Nunc risus nulla, lobortis quis sapien id, laoreet rutrum odio. Donec eleifend viverra tellus, nec tincidunt justo congue eu.
-						</p>
-					</div>
-				</div>
-			</div>
-
-		</div>
-		<div class="row">
-			<div class="col-sm bg-light" style="height: 900px">
-				
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm bg-nav" style="height: 300px">
+		<div id="contactus" class="row h-25">
+			<div class="col-sm bg-dark">
 				
 			</div>
 		</div>
@@ -93,7 +89,7 @@
 			<li class="nav-item"><a href="#home" class="nav-link active"><i class="fa fa-circle"></i></a></li>
 			<li class="nav-item"><a href="#whatwedo" class="nav-link"><i class="fa fa-circle"></i></a></li>
 			<li class="nav-item"><a href="#ourstack" class="nav-link"><i class="fa fa-circle"></i></a></li>
-			<li class="nav-item"><a href="#04-third-content" class="nav-link"><i class="fa fa-circle"></i></a></li>
+			<li class="nav-item"><a href="#contactus" class="nav-link"><i class="fa fa-circle"></i></a></li>
 		</ul>
 	</div>
 @endsection
