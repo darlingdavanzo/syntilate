c6c1e858565d8cf25f1c0d9b0a4e4528797723e5
darlingfenix <stormofdark@libero.it>
2019-06-26 12:25:40 +0200

    Added footer

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -204,6 +204,65 @@
 				</div>
 			</div>
 		</div>
+		<div class="row">
+			<div id="footer" class="container-fluid bg-footer text-white">
+				<div class="row pt-5">
+						<div class="col-sm">					
+							<img class="img-fluid" src="{{ @asset('assets/images/syntilate.png') }}">			
+						</div>
+						<div class="col-sm">					
+							<h5>Links</h5>
+							<ul class="list-unstyled">
+								<li>
+									<a class="link-footer" href="#"><i class="fa fa-angle-right"></i> Home</a>
+								</li>
+								<li>
+									<a class="link-footer" href="#"><i class="fa fa-angle-right"></i> Privacy Policy</a>
+								</li>
+								<li>
+									<a class="link-footer" href="#"><i class="fa fa-angle-right"></i> Chi Siamo</a>
+								</li>
+								<li>
+									<a class="link-footer" href="#"><i class="fa fa-angle-right"></i> Servizi</a>
+								</li>
+								<li>
+									<a class="link-footer" href="#"><i class="fa fa-angle-right"></i> Contattaci</a>
+								</li>
+								
+								
+								
+							</ul>
+						</div>
+						<div class="col-sm">					
+							<h5>Contattaci</h5>
+							<i class="fa fa-envelope mr-2"></i> info@syntilate.com
+							<br>
+							<br>
+							<i class="fa fa-phone mr-2"></i> +39 346 957 1812
+						</div>
+						<div class="col-sm">					
+							<h5>Social</h5>
+							<ul class="list-unstyled list-inline pt-3">
+								<li class="list-inline-item">
+									<a class="link-footer-social" href="#"><i class="fa fa-2x fa-facebook"></i></a>
+								</li>
+								<li class="list-inline-item ml-3 mr-3">
+									<a class="link-footer-social" href="#"><i class="fa fa-2x fa-twitter"></i></a>
+								</li>
+								<li class="list-inline-item">
+									<a class="link-footer-social" href="#"><i class="fa fa-2x fa-instagram"></i></a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="row text-center mt-3" style="background-color: #000">
+						<div class="col-sm">					
+							<p class="small pt-3">© 2019 Syntilate Tutti i diritti riservati |  Via Primo Maggio 156, AN 60131, Ancona, Italy |  P.I. 01820200705 02818440428 </p>
+						</div>
+					</div>
+				</div>
+			</div>			
+		</div>
 	</div>
 @endsection
 
