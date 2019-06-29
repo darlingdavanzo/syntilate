7bffbfb760edc4322124a5ed9d7f1e4a42136b39
Darling D'Avanzo <stormofdark@libero.it>
2019-06-29 22:32:51 +0200

    call to action button

--- resources/views/pages/landing/sections/_calltoaction.blade.php
+++ resources/views/pages/landing/sections/_calltoaction.blade.php
@@ -3,6 +3,6 @@
 		<p class="subtext-header-section text-white" style="font-weight: 500;">Se anche tu sei alla ricerca di software di valore, ad alto contenuto tecnologico e che rispetti i piu alti standard qualitativi presenti sul mercato richiedi subito un preventivo</p>
 
 		<hr class="col-md-3 mb-5 mt-5 border-top border-white"/>
-		<a class="btn btn-lg btn-info mb-2 ml-auto" href="#preventivo">RICHIEDI</a>
+		<a class="btn btn-lg btn-outline-light mb-2 ml-auto" href="#preventivo">RICHIEDI</a>
 	</div>
 </div>
