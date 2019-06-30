c1c4c74786919fe1b02c4a11c4d30cff823b7533
Darling D'Avanzo <stormofdark@libero.it>
2019-06-30 22:30:23 +0200

    added meta desc

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
 	<meta name="csrf-token" id="token" content="{{ csrf_token() }}">
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Syntilate è una software house operante nel settore del software engineering, web & mobile development e IT consulting. Hai bisogno di una soluzione software ? Richiedi subito un preventivo gratuito!" />
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
