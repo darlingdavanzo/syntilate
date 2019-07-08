a188eceef8c5bc9b4d9d290fac6548460d55dda8
darlingfenix <stormofdark@libero.it>
2019-07-08 12:22:41 +0200

    fixed cookie policy

--- public/js/iubenda-config.js
+++ public/js/iubenda-config.js
@@ -3,6 +3,7 @@ _iub.csConfiguration = {
 	"lang":"it",
 	"siteId":1627250,
 	"cookiePolicyId":33678666, 
+	"cookiePolicyUrl": "https://www.iubenda.com/privacy-policy/33678666/legal",
 	"banner": { 
 		"acceptButtonDisplay":true,
 		"customizeButtonDisplay":true,
