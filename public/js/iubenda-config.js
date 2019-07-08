69687f966bf81f0d3aa8edbecc334fa24ac1e36e
darlingfenix <stormofdark@libero.it>
2019-07-08 12:14:57 +0200

    updated privacy

--- public/js/iubenda-config.js
+++ public/js/iubenda-config.js
@@ -1,8 +1,8 @@
 var _iub = _iub || [];
 _iub.csConfiguration = {
 	"lang":"it",
-	"siteId":1616509,
-	"cookiePolicyId":32123665, 
+	"siteId":1627250,
+	"cookiePolicyId":33678666, 
 	"banner": { 
 		"acceptButtonDisplay":true,
 		"customizeButtonDisplay":true,
